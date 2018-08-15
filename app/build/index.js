47eba2e25d0f58867eecd0801b684abdc87fe7c6
weiyaodong <704246158@qq.com>
2018-08-15 13:11:24 +0800

    init

---
+++ app/build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var App = require("./app");
+var greeter = new App.Controller("Whatup");
+greeter.greet();
+App.defaultGreeter().greet();
+var a = 0;
+if (a == 1) {
+}
+var c = 1;
---
+++ app/src/index.ts
@@ -0,0 +1,14 @@
+import * as App from "./app"
+
+var greeter = new App.Controller("Whatup");
+
+greeter.greet();
+App.defaultGreeter().greet(); 
+
+let a: number = 0;
+
+if (a == 1) {
+    
+}
+
+let c = 1; 
